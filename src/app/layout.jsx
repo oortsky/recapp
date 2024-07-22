a32cefafe056f2b332a434b3922dd6a4e3f14f88
oortsky <bayuaprio22@gmail.com>
2024-07-22 18:37:39 +0700

    Update (PWA Integrated)

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -1,6 +1,6 @@
 import { Poppins } from "next/font/google";
-import TopNavbar from "../components/TopNavbar.jsx"
-import BottomNavbar from "../components/BottomNavbar.jsx"
+import TopNavbar from "../components/TopNavbar.jsx";
+import BottomNavbar from "../components/BottomNavbar.jsx";
 import "./globals.css";
 
 const poppins = Poppins({
@@ -10,17 +10,33 @@ const poppins = Poppins({
 
 export const metadata = {
   title: "Recapp.",
-  description:
-    "an application to recap data from rubber goods • create by Axara"
+  description: "Recap app for goods management.",
+  generator: "Next.js",
+  manifest: "/manifest.json",
+  keywords: ["nextjs", "nextjs13", "next13", "pwa", "next-pwa"],
+  themeColor: [{ media: "(prefers-color-scheme: dark)", color: "#fff" }],
+  authors: [
+    { name: "Bayu Aprio Pamungkas" },
+    {
+      name: "Bayu Aprio Pamungkas",
+      url: "https://www.github.com/oortsky/"
+    }
+  ],
+  viewport:
+    "minimum-scale=1, initial-scale=1, width=device-width, shrink-to-fit=no, viewport-fit=cover",
+  icons: [
+    { rel: "apple-touch-icon", url: "icons/icon-128x128.png" },
+    { rel: "icon", url: "icons/icon-128x128.png" }
+  ]
 };
 
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body className={poppins.className}>
-<TopNavbar />
+        <TopNavbar />
         {children}
-<BottomNavbar />
+        <BottomNavbar />
       </body>
     </html>
   );
